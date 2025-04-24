f3751e9e98854d23da667b6f31f57ef4e567756c
Lahcen <echcharyilahcen@gmail.com>
2025-04-24 23:01:36 +0100

    Adding the CpuMonitor class structure

---
+++ src/CpuMonitor.cpp
@@ -0,0 +1,17 @@
+#include "CpuMonitor.h"
+
+// Constructeur
+CpuMonitor::CpuMonitor() {
+    // Initialisation des ressources si nécessaire
+}
+
+// Méthode pour récupérer l'utilisation du CPU (sera implémentée plus tard)
+double CpuMonitor::getCpuUsage() {
+    // Retourner une valeur factice pour l'instant (par exemple, 0.0)
+    return 0.0;
+}
+
+// Destructeur
+CpuMonitor::~CpuMonitor() {
+    // Libération des ressources si nécessaire
+}
