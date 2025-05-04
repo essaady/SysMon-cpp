ac0a8a865008715840d92a12982ea16e174b5280
badie16 <badi3bahida16@gmail.com>
2025-05-04 18:25:35 +0100

    add build

---
+++ build/CMakeFiles/SysMon-cpp.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SysMon-cpp.dir/src/CpuMonitor.cpp.o"
+  "CMakeFiles/SysMon-cpp.dir/src/CpuMonitor.cpp.o.d"
+  "CMakeFiles/SysMon-cpp.dir/src/MemoryMonitor.cpp.o"
+  "CMakeFiles/SysMon-cpp.dir/src/MemoryMonitor.cpp.o.d"
+  "CMakeFiles/SysMon-cpp.dir/src/ProcessMonitor.cpp.o"
+  "CMakeFiles/SysMon-cpp.dir/src/ProcessMonitor.cpp.o.d"
+  "CMakeFiles/SysMon-cpp.dir/src/SysMon.cpp.o"
+  "CMakeFiles/SysMon-cpp.dir/src/SysMon.cpp.o.d"
+  "CMakeFiles/SysMon-cpp.dir/src/main.cpp.o"
+  "CMakeFiles/SysMon-cpp.dir/src/main.cpp.o.d"
+  "SysMon-cpp"
+  "SysMon-cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SysMon-cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
