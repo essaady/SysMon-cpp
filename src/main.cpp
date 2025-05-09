57c2278953165bdb33ca9bbb6785a5a0d350918f
2005-noun <noumidia.ounjime.43@edu.uiz.ac.ma>
2025-05-09 21:39:52 +0100

    CpuMonitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,9 @@
-#include "SysMon.h"
-#include "ProcessMonitor.h"
 #include <iostream>
 #include "CpuMonitor.h"
+
 int main() {
-    auto topProcesses = ProcessMonitor::getTopProcesses(5);
-    
-    std::cout << std::left << std::setw(8) << "PID" 
-            << std::setw(30) << "COMMAND" 
-            << "CPU %" << std::endl;
-    std::cout << std::string(45, '-') << std::endl;
-
-    for (const auto& proc : topProcesses) {
-        std::cout << std::left << std::setw(8) << proc.pid
-                << std::setw(30) << 
-                (proc.command.length() > 28 ? 
-                proc.command.substr(0, 25) + "..." : proc.command)
-                << std::fixed << std::setprecision(1) << proc.cpu_usage << "%" 
-                << std::endl;
-    }
-
-
+    CpuMonitor monitor;
+    double usage = monitor.getUsage();
+    std::cout << "Utilisation du CPU : " << usage << "%" << std::endl;
     return 0;
 }
