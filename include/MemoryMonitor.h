e914ba1a3892cdc084110992d620bfe878dccc96
Hamzatahiri-coder <hamzatechnologue@gmail.com>
2025-05-18 23:04:07 +0200

    Update MemoryMonitor.h

--- include/MemoryMonitor.h
+++ include/MemoryMonitor.h
@@ -1,40 +1,9 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-//Hamza Tahiri
+#ifndef MEMORY_MONITOR_H
+#define MEMORY_MONITOR_H
 
-void afficherUtilisationMemoire() {
-    std::ifstream fichier("/proc/meminfo");
-    if (!fichier) {
-        std::cerr << "Erreur : Impossible d'ouvrir /proc/meminfo" << std::endl;
-        return;
-    }
+class MemoryMonitor {
+public:
+    static void afficherUtilisationMemoire();
+};
 
-    std::string ligne;
-    unsigned long memTotal = 0, memLibre = 0, buffers = 0, cached = 0;
-
-    while (std::getline(fichier, ligne)) {
-        std::istringstream iss(ligne);
-        std::string cle;
-        unsigned long valeur;
-        std::string unite;
-
-        iss >> cle >> valeur >> unite;
-
-        if (cle == "MemTotal:") memTotal = valeur;
-        else if (cle == "MemFree:") memLibre = valeur;
-        else if (cle == "Buffers:") buffers = valeur;
-        else if (cle == "Cached:") cached = valeur;
-
-        if (memTotal && memLibre && buffers && cached)
-            break;
-    }
-
-    unsigned long memUtilisee = memTotal - memLibre - buffers - cached;
-    double pourcentage = (double)memUtilisee / memTotal * 100;
-
-    std::cout << "Mémoire utilisée : " << memUtilisee / 1024 << " MB / "
-              << memTotal / 1024 << " MB ("
-              << pourcentage << "%)" << std::endl;
-}
+#endif // MEMORY_MONITOR_H
