7047a42295fc43f58829b57cc7c9db16b12e7a8f
sofyanehb <sofyanhab2024@gmail.com>
2025-05-31 23:18:52 +0100

    Add refresh loop every 2 seconds

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <chrono>   // for std::chrono::seconds
+#include <thread>   // for std::this_thread::sleep_for
+
+void displayInfo() {
+    
+    // Put your code here to display updated information
+    std::cout << "Updating info..." << std::endl;
+}
+
+int main() {
+    while (true) {
+
+        system("clear"); // Use "CLS" instead of "clear" on Windows
+        displayInfo();
+
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+    return 0;
+}
