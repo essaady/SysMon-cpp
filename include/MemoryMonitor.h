fe5359c115b15972980020dd224e488d133d8b82
Anas <berkaanas@gmail.com>
2025-06-03 17:29:32 +0100

    Fix base initial version

--- include/MemoryMonitor.h
+++ include/MemoryMonitor.h
@@ -1,39 +1,49 @@
-#ifndef MEMORY_MONITOR_H
-#define MEMORY_MONITOR_H
+#pragma once
+#ifndef _MEMORYMONITOR_H
+#define _MEMORYMONITOR_H
 
 #include <string>
-#include <map>
+#include <vector>
 
-#include "./SysMon.h"
-
-class MemInfo : public SysMon{
-public:
-    size_t memUsage(int log);
-};
+typedef struct ram{
+    float usage;
+    float freeMem;
+    float usageSwp;
+    float freeSwp;
+    std::size_t totalMemInMb;
+    std::size_t SwapMeminMb;
+} ram;
 
 class MemoryMonitor {
+protected:
+    ram RAM;
+    std::vector<std::string> memInfo;
+
 public:
     MemoryMonitor();
+    
     ~MemoryMonitor();
 
     bool update();
 
     unsigned long long getTotalMemory() const;
+
     unsigned long long getFreeMemory() const;
+    
     unsigned long long getUsedMemory() const;
+    
     double getMemoryUsagePercentage() const;
 
     unsigned long long getTotalSwap() const;
+    
     unsigned long long getFreeSwap() const;
+    
     unsigned long long getUsedSwap() const;
+    
     double getSwapUsagePercentage() const;
 
-    std::string exportAsText() const;
-    std::string exportAsCSV() const;
+    std::size_t memUsage(int log);
 
-private:
-    std::map<std::string, unsigned long long> memInfo;
-    bool readMemInfo();
 };
 
 #endif
--- src/SysMon.cpp
+++ src/SysMon.cpp
@@ -1,27 +1,63 @@
 #include "../include/SysMon.h"
+#include <unistd.h>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+SysMon::SysMon(int updateInterval, bool fullLog): CpuMonitor(), MemoryMonitor(), ProcessMonitor(){
+    this->updateInterval=updateInterval;
+    this->fullLog=fullLog;
+}
+
+SysMon::~SysMon(){}
+
+
+int SysMon::run(int limit){
+    int i = 0;
+    while (i < limit)
+    {
+        usleep(updateInterval);
+        if(!fullLog){
+            system("clear");
+        }
+        update();
+        i++;
+    }
+    return 0;
+}
+
+bool SysMon::update(){
+    CpuMonitor::update();
+    ProcessMonitor::update();
+    MemoryMonitor::update();
+    return true;
+}
 
 //function to get the current time for loggin.
-char *SysMon::getTime()
+string SysMon::getTime()
 {
     time_t timestamp;
     time(&timestamp);
-    char *time = ctime(&timestamp);
+    char* time = ctime(&timestamp);
 
     if (time[strlen(time) - 1] == '\n')
         time[strlen(time) - 1] = '\0';
     return time;
 }
 
-//Fetching required files '/proc/stat/' and '/proc/meminfo'.
-std::string SysMon::getInfo(std::string _file_path)
+// //Fetching required files '/proc/stat/' and '/proc/meminfo'.
+string SysMon::getInfo(string _file_path)
 {
-    std::string content;
-    std::stringstream content_stream;
+    string content;
+    stringstream content_stream;
 
-    std::fstream info(_file_path, std::ios::in);
+    fstream info(_file_path, ios::in);
     if (!info)
     {
-        std::cerr << "There was an error opening the file" << std::endl;
+        cerr << "There was an error opening the file" << endl;
         return " ";
     }
     content_stream << info.rdbuf();
@@ -30,9 +66,37 @@ std::string SysMon::getInfo(std::string _file_path)
     return content;
 }
 
-void SysMon::log(std::ostream &out)
+void SysMon::log(ostream &out)
 {
-    std::fstream logFile("log.txt", std::ios::app);
+    fstream logFile("log.txt", ios::app);
     logFile << out.rdbuf();
     logFile.close();
+}
+
+std::vector<std::string> getVector(std::istringstream &iss)
+{
+    int i = 0;
+    std::vector<std::string> info;
+    std::string content;
+    while (iss >> content && i < 13)
+    {
+        info.push_back(content);
+        i++;
+    }
+
+    return info;
+}
+
+bool isNumber(std::string &string)
+{
+    bool isNumber = true;
+    for (auto str : string)
+    {
+        if (!std::isdigit(str))
+        {
+            isNumber = false;
+            break;
+        }
+    }
+    return isNumber;
 }
