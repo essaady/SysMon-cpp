f9e847e2f013f6ff5f10d9ac0cec84f508730d17
Aicha EL KHDDAR <aichaelkhddar03@gmail.com>
2025-06-03 23:07:53 +0100

    Update main.cpp
    updated version: main.cpp (reference: diagramme UML partagé)

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,59 @@
 #include <iostream>
 #include <thread> //pour utiliser std::this_thread::sleep_for() (mise en pause du programme)
-#include "../include/MemoryMonitor.h"
 #include <chrono> // pour utiliser chrono dans sleep_for
+#include "../include/MemoryMonitor.h"
+#include <iomanip>  //pour setprecision et fixed, voir https://devdocs.io/cpp/io/manip/setprecision et https://devdocs.io/cpp/io/manip/fixed
 
 using namespace std;
 
 int main() {
-
     MemoryMonitor memMonitor;
 
-    cout << "SysMon-cpp - Moniteur de memoire" << endl;
-    cout << "--------------------------------" << endl;
+    cout << "SysMon-cpp - MEMORY MONITORING" << endl;
+    cout << "------------------------------" << endl;
 
-    //boucle principale pour mettre a jour et afficher les informations
-    //affiche les informations toutes les 2 secondes indefiniment
+    //configuration de la precision pour tous les nombres flottants
+    cout << fixed << setprecision(1);
+    /*
+    ou on peut utiliser aussi:
+    cout.precision(1);
+    cout<<fixed;
+    mais sans #include <iomanip>, voir https://devdocs.io/cpp/io/ios_base/precision
+    */
     while (true) {
-        //mettre a jour les donnees de memoire
         if (memMonitor.update()) {
-            //afficher l'heure actuelle
-            time_t now = time(0);
-            char* dt = ctime(&now);
-            cout << "Mise a jour: " << dt;
+            //convertir ram en GB
+            const double totalRAM = memMonitor.getTotalMemory() / (1024.0 * 1024.0);
+            const double usedRAM = memMonitor.getUsedMemory() / (1024.0 * 1024.0);
+            const double freeRAM = memMonitor.getFreeMemory() / (1024.0 * 1024.0);
+            
+            //affichage de la ram
+            cout << "___RAM___" << endl;
+            cout << "Total RAM:    " << totalRAM << " GB" << endl;
+            cout << "Used RAM:  " << usedRAM << " GB / " << totalRAM << " GB" << endl;
+            cout << "Free RAM:    " << freeRAM << " GB / " << totalRAM << " GB" << endl;
+            cout << "Use:    " << memMonitor.getMemoryUsagePercentage() << "%" << endl << endl;
 
-            //afficher les informations de memoire
-            memMonitor.display();
+            //convertir swap en GB
+            const double totalSwap = memMonitor.getTotalSwap() / (1024.0 * 1024.0);
+            const double usedSwap = memMonitor.getUsedSwap() / (1024.0 * 1024.0);
+            const double freeSwap = memMonitor.getFreeSwap() / (1024.0 * 1024.0);
+            
+            //affichage swap
+            if (totalSwap > 0) {
+                cout << "___SWAP___" << endl;
+                cout << "Total Swap:    " << totalSwap << " GB" << endl;
+                cout << "Used Swap:  " << usedSwap << " GB / " << totalSwap << " GB" << endl;
+                cout << "Free Swap:    " << freeSwap << " GB / " << totalSwap << " GB" << endl;
+                cout << "Use:    " << memMonitor.getSwapUsagePercentage() << "%" << endl << endl;
+            }
+
         } else {
             cout << "Erreur lors de la mise a jour des informations de memoire." << endl;
         }
 
         //attendre 2 secondes avant la prochaine mise a jour
-        this_thread::sleep_for(chrono::seconds(2)); //l'utilisation de la fct chrono est naicessaire, voir la documentaion de la fct std::this_thread::sleep_for en https://devdocs.io/cpp/thread/sleep_for
-    }
+        this_thread::sleep_for(chrono::seconds(2)); //l'utilisation de la fct chrono est naicessaire, voir la documentaion de la fct std::this_thread::sleep_for en https://devdocs.io/cpp/thread/sleep_for    }
 
     return 0;
 }
