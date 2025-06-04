54079298d2c693de3197ac89070d1f64013a8626
Aicha EL KHDDAR <aichaelkhddar03@gmail.com>
2025-06-04 12:23:57 +0100

    Update MemoryMonitor.h

--- include/MemoryMonitor.h
+++ include/MemoryMonitor.h
@@ -1,32 +1,34 @@
-#ifndef MEMORY_MONITOR_H
-#define MEMORY_MONITOR_H
+#ifndef MEMORYMONITOR_H
+#define MEMORYMONITOR_H
 
 #include <string>
+#include <vector>
 using namespace std;
 
 class MemoryMonitor {
 private:
-    unsigned long total_memory;    //memoire totale en KB
-    unsigned long free_memory;     //memoire libre en KB
-    bool readMemoryInfo(); //lire les donnees de memoire depuis /proc/meminfo
+    struct ram {
+        size_t totalMemInMb;
+        float usage;
+        float freeMem;
+        size_t SwapMeminMb;
+        float usageSwp;
+        float freeSwp;
+    } RAM;
+    vector<string> memInfo;
 
 public:
-    //constructeur
-    MemoryMonitor();
-
-    //mettre a jour les donnees de memoire
     bool update();
-
-    //methodes pour obtenir les informations de memoire
     unsigned long getTotalMemory() const;
     unsigned long getFreeMemory() const;
     unsigned long getUsedMemory() const;
-
-    //affiche les informations de memoire
-    void display() const;
-
-    //formater les valeurs de memoire en GB
-    string formatMemory(unsigned long memory_kb) const;
+    double getMemoryUsagePercentage() const;
+    unsigned long getTotalSwap() const;
+    unsigned long getFreeSwap() const;
+    unsigned long getUsedSwap() const;
+    double getSwapUsagePercentage() const;
 };
+
+//le diagramme UML était respecté d'une manière stricte 
 
 #endif
