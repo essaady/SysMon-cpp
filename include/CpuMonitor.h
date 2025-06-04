28efa83fcc8654cefd1ca5d7a96e9f9000db4f8e
Intissar Ait hssain <intissar.aithssain.39@edu.uiz.ac.ma>
2025-06-04 15:32:20 +0200

    Update CpuMonitor.h

--- include/CpuMonitor.h
+++ include/CpuMonitor.h
@@ -1,21 +1,33 @@
 #ifndef CPUMONITOR_H
 #define CPUMONITOR_H
 
-#include <vector>
 #include <string>
+#include <vector>
+
+struct CPU {
+    float usagePerCPU;
+    float frequency;
+    float frequencyMax;
+};
 
 class CpuMonitor {
 public:
     CpuMonitor();
 
-    bool readCpuStats(std::vector<unsigned long long>& stats);
-
-
-    double getCpuUsage();
+    bool update();             // Met à jour les stats CPU
+    float getCpuUsage();       // Renvoie le pourcentage d'utilisation globale
+    float getCpuFreq();        // Renvoie la fréquence actuelle
+    std::string getCpuInfo();  // Renvoie une chaîne d'information détaillée
 
 private:
+    short nbrCPU;
+    std::vector<CPU> cpus;
     std::vector<unsigned long long> prevStats;
     bool firstRead;
+
+    bool readCpuStats(std::vector<unsigned long long>& stats);
+    float getFrequency();
+    float getMaxFrequency();
 };
 
-#endif
+#endif // CPUMONITOR_H
