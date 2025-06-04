b09de6009180acf2e1a2a7192ef0deddee0defcc
Aicha EL KHDDAR <aichaelkhddar03@gmail.com>
2025-06-04 02:06:27 +0100

    Update MemoryMonitor.h
    update respecte le diagramme UML

--- include/MemoryMonitor.h
+++ include/MemoryMonitor.h
@@ -1,34 +1,32 @@
-#ifndef MEMORY_MONITOR_H
-#define MEMORY_MONITOR_H
+#ifndef MEMORYMONITOR_H
+#define MEMORYMONITOR_H
 
 #include <string>
 #include <vector>
 using namespace std;
 
-struct ram {
-    size_t totalMemInMb;
-    float usage;
-    float freeMem;
-    size_t SwapMemInMb;
-    float usageSwp;
-    float freeSwp;
-};
-
 class MemoryMonitor {
 private:
-    ram RAM;
+    struct ram {
+        size_t totalMemInMb;
+        float usage;
+        float freeMem;
+        size_t SwapMeminMb;
+        float usageSwp;
+        float freeSwp;
+    } RAM;
     vector<string> memInfo;
 
 public:
     bool update();
-    unsigned long getTotalMemory();
-    unsigned long getFreeMemory();
-    unsigned long getUsedMemory();
-    double getMemoryUsagePercentage();
-    unsigned long getTotalSwap();
-    unsigned long getFreeSwap();
-    unsigned long getUsedSwap();
-    double getSwapUsagePercentage();
+    unsigned long getTotalMemory() const;
+    unsigned long getFreeMemory() const;
+    unsigned long getUsedMemory() const;
+    double getMemoryUsagePercentage() const;
+    unsigned long getTotalSwap() const;
+    unsigned long getFreeSwap() const;
+    unsigned long getUsedSwap() const;
+    double getSwapUsagePercentage() const;
 };
 
 #endif
