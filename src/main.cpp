b8c75dc60c63913ee726d3dde0aa7934ef2da632
Gdaa Jaber <jabergdaa18@gmail.com>
2025-06-04 15:52:55 +0100

    Ajout d'une boucle d'actualisation toutes les 2 secondes pour l'affichage CLI

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <iomanip>
+#include <thread>
+#include <chrono>
 
 void printHeader() {
     std::cout << std::left
@@ -20,12 +22,19 @@ void printData(float cpuUsage, double memUsed, int procCount) {
 }
 
 int main() {
-    float cpuUsage = 35.7f;
-    double memUsed = 4200.5;
-    int procCount = 123;
+    while (true) {
+        float cpuUsage = 35.7f;    
+        double memUsed = 4200.5;
+        int procCount = 123;
 
-    printHeader();
-    printData(cpuUsage, memUsed, procCount);
+        std::cout << "\033[2J\033[H";  
+
+        printHeader();
+        printData(cpuUsage, memUsed, procCount);
+
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    }
 
     return 0;
 }
