b4466ef1f300b9e01cd3ff4377dc111f3744c30a
Intissar Ait hssain <intissar.aithssain.39@edu.uiz.ac.ma>
2025-06-04 15:33:43 +0200

    Update CpuMonitor.cpp

--- src/CpuMonitor.cpp
+++ src/CpuMonitor.cpp
@@ -4,8 +4,12 @@
 #include <thread>
 #include <chrono>
 #include <iostream>
+#include <iomanip>
 
-CpuMonitor::CpuMonitor() : firstRead(true) {}
+CpuMonitor::CpuMonitor() : firstRead(true) {
+    nbrCPU = std::thread::hardware_concurrency();
+    cpus.resize(nbrCPU);
+}
 
 bool CpuMonitor::readCpuStats(std::vector<unsigned long long>& stats) {
     std::ifstream file("/proc/stat");
@@ -16,13 +20,12 @@ bool CpuMonitor::readCpuStats(std::vector<unsigned long long>& stats) {
 
     std::string line;
     while (std::getline(file, line)) {
-        if (line.compare(0, 3, "cpu") == 0 && (line.size() == 3 || line[3] == ' ')) {
+        if (line.rfind("cpu ", 0) == 0) {
             std::istringstream ss(line);
-            std::string cpuLabel;
-            ss >> cpuLabel;
-
-            unsigned long long value;
+            std::string label;
+            ss >> label;
             stats.clear();
+            unsigned long long value;
             while (ss >> value) {
                 stats.push_back(value);
             }
@@ -32,29 +35,26 @@ bool CpuMonitor::readCpuStats(std::vector<unsigned long long>& stats) {
     return false;
 }
 
-double CpuMonitor::getCpuUsage() {
+bool CpuMonitor::update() {
     std::vector<unsigned long long> currentStats;
 
     if (!readCpuStats(currentStats)) {
-        return -1.0;
+        return false;
     }
 
     if (firstRead) {
         prevStats = currentStats;
         firstRead = false;
-
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         if (!readCpuStats(currentStats)) {
-            return -1.0;
+            return false;
         }
     }
-
 
     unsigned long long prevIdle = prevStats[3] + prevStats[4];
     unsigned long long idle = currentStats[3] + currentStats[4];
 
-    unsigned long long prevNonIdle = 0;
-    unsigned long long nonIdle = 0;
+    unsigned long long prevNonIdle = 0, nonIdle = 0;
 
     for (size_t i = 0; i < prevStats.size(); ++i) {
         if (i != 3 && i != 4) {
@@ -71,8 +71,52 @@ double CpuMonitor::getCpuUsage() {
 
     prevStats = currentStats;
 
-    if (totald == 0) return 0.0;
+    cpus[0].usagePerCPU = (totald != 0) ? ((float)(totald - idled) / totald * 100.0f) : 0.0f;
+    cpus[0].frequency = getFrequency();
+    cpus[0].frequencyMax = getMaxFrequency();
 
-    double cpuPercentage = (double)(totald - idled) / totald * 100.0;
-    return cpuPercentage;
+    return true;
+}
+
+float CpuMonitor::getCpuUsage() {
+    return cpus[0].usagePerCPU;
+}
+
+float CpuMonitor::getCpuFreq() {
+    return cpus[0].frequency;
+}
+
+std::string CpuMonitor::getCpuInfo() {
+    std::ostringstream info;
+    info << std::fixed << std::setprecision(2);
+    info << "Nombre de cœurs : " << nbrCPU << "\n";
+    info << "Utilisation : " << getCpuUsage() << " %\n";
+    info << "Fréquence actuelle : " << getCpuFreq() << " MHz\n";
+    info << "Fréquence maximale : " << cpus[0].frequencyMax << " MHz\n";
+    return info.str();
+}
+
+float CpuMonitor::getFrequency() {
+    std::ifstream file("/proc/cpuinfo");
+    std::string line;
+    while (std::getline(file, line)) {
+        if (line.find("cpu MHz") != std::string::npos) {
+            std::istringstream ss(line);
+            std::string label;
+            float freq;
+            ss >> label >> label >> freq;
+            return freq;
+        }
+    }
+    return 0.0f;
+}
+
+float CpuMonitor::getMaxFrequency() {
+    std::ifstream file("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq");
+    if (file.is_open()) {
+        int khz;
+        file >> khz;
+        return khz / 1000.0f;
+    }
+    return 0.0f;
 }
