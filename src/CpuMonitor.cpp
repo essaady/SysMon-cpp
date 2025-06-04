72e401aace98febcd67e041c800f22e32ba80c97
Aicha EL KHDDAR <aichaelkhddar03@gmail.com>
2025-06-04 12:31:24 +0100

    Update CpuMonitor.cpp

--- src/CpuMonitor.cpp
+++ src/CpuMonitor.cpp
@@ -1,90 +1,86 @@
-#include "../include/MemoryMonitor.h"
-#include <iostream>
+#include "../include/CpuMonitor.h"
 #include <fstream> //gestion des fichiers (on utilise ifstream)
 #include <sstream> //manipuler les flux de chaines de caracteres (istringstream et ostringstream)
+#include <thread> //pour utiliser std::this_thread::sleep_for()
+#include <chrono> //pour utiliser chrono dans sleep_for
+#include <iostream>
 
 using namespace std;
 
-bool MemoryMonitor::update() {
-    ifstream meminfo("/proc/meminfo"); //ifstream pour ouvrir le fichier "/proc/meminfo" en lecture
-    if (!meminfo.is_open()) { //verifier si le fichier a bien ete ouvert
-        cout << "Erreur: Impossible d'ouvrir /proc/meminfo" << endl;
+bool CpuMonitor::update() {
+    //lecture
+    ifstream stat1("/proc/stat"); //ifstream pour ouvrir le fichier "/proc/stat" en lecture
+    if (!stat1.is_open()) { //verifier si le fichier a bien ete ouvert
+        cout << "erreur: impossible d'ouvrir /proc/stat" << endl;
         return false;
     }
 
-    //vider le vecteur memInfo avant de le remplir
-    memInfo.clear();
-    
     string line;
-    unsigned long total_memory = 0, free_memory = 0, total_swap = 0, free_swap = 0;
-    
-    while (getline(meminfo, line)) { //lire le fichier ligne par ligne
-        memInfo.push_back(line); //stocker chaque ligne dans le vecteur
-        
-        istringstream iss(line); //analyser et decouper la ligne pour extraire facilement les mots (flux d'entree depuis une chaine de caracteres)
-        string key;
-        unsigned long value;
+    getline(stat1, line); //lire 1er ligne (cpu global)
+    stat1.close(); //fermer le fichier quand la lecture terminee
 
-        //lecture de la cle et de la valeur
-        iss >> key >> value; //iss lit depuis la ligne et extrait le premier mot dans key et le second (valeur numerique) dans value
+    //extraire les valeurs de la ligne
+    istringstream iss1(line); //analyser et decouper la ligne pour extraire facilement les mots (flux d'entree depuis une chaine de caracteres)
+    string cpu_label;
+    long long user1, nice1, system1, idle1;
+    iss1 >> cpu_label >> user1 >> nice1 >> system1 >> idle1; //iss1 lit depuis la ligne et extrait le 1er mot dans cpu_label et les valeurs numirics d'apres dans user1, nice1, system1, idle1
+    long long total1 = user1 + nice1 + system1 + idle1;
 
-        //extraction des informations necessaires
-        if (key == "MemTotal:") {
-            total_memory = value;
-        } else if (key == "MemFree:") {
-            free_memory = value;
-        } else if (key == "SwapTotal:") {
-            total_swap = value;
-        } else if (key == "SwapFree:") {
-            free_swap = value;
-        }
+    //attendre 1s pour 2eme mesure
+    this_thread::sleep_for(chrono::seconds(1)); //c la fct std::this_thread::sleep_for pour une pause de 1s, et chrono pour la gestion du temps (documentation deja mentionner dans main.cpp, memoryMonitor)
+
+    //2eme lecture
+    ifstream stat2("/proc/stat");
+    if (!stat2.is_open()) {
+        cout << "erreur: impossible d'ouvrir /proc/stat" << endl;
+        return false;
     }
 
-    //mettre a jour la structure RAM
-    RAM.totalMemInMb = total_memory / 1024; //convertir de KB a MB
-    RAM.freeMem = static_cast<float>(free_memory) / 1024.0f; //convertir de KB a MB
-    // static_cast est un operateur pour faire le casting
-    //suffixe f (dans 1024.0f) indique que la constante est de type float, sans f le compilateur utilise double par defaut (32bits vs 64bits)
-    RAM.usage = static_cast<float>(total_memory - free_memory) / 1024.0f; //memoire utilisee en MB
-    
-    RAM.SwapMemInMb = total_swap / 1024; //convertir de KB a MB
-    RAM.freeSwp = static_cast<float>(free_swap) / 1024.0f; //convertir de KB a MB
-    RAM.usageSwp = static_cast<float>(total_swap - free_swap) / 1024.0f; //swap utilisee en MB
+    getline(stat2, line);
+    stat2.close();
 
-    meminfo.close(); //fermer le fichier quand la lecture terminee
+    //extraire les valeurs
+    istringstream iss2(line);
+    string cpu_labe2;
+    long long user2, nice2, system2, idle2;
+    iss2 >> cpu_labe2 >> user2 >> nice2 >> system2 >> idle2;
+    long long total2 = user2 + nice2 + system2 + idle2;
+
+    //calculer les differences
+    long long total_diff = total2 - total1;
+    long long idle_diff = idle2 - idle1;
+
+    //calculer le pourcentage d'utilisation
+    if (total_diff > 0) {
+        CPU.usageCPU = ((float)(total_diff - idle_diff) / total_diff) * 100.0f; 
+        //(float) est un casting avant la division
+        //suffixe f (dans 100.0f) force le type float, sans f c'est un double par defaut (32bits vs 64bits)
+    } else {
+        CPU.usageCPU = 0.0f; //initialise usageCPU a 0 en float
+    }
+
     return true;
 }
 
-unsigned long MemoryMonitor::getTotalMemory() {
-    return RAM.totalMemInMb * 1024; //en KB
+float CpuMonitor::getCpuUsage() const {
+    return CPU.usageCPU; //retourne le pourcentage d'utilisation
 }
 
-unsigned long MemoryMonitor::getFreeMemory() {
-    return static_cast<unsigned long>(RAM.freeMem * 1024); //en KB
+float CpuMonitor::getCpuFreq() const {
+    //lecture de la frequence actuelle depuis /sys
+    ifstream cpuinfo("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq");
+    if (!cpuinfo.is_open()) {
+        return 0.0f; //retourne 0 si impossible de lire
+    }
+
+    float freq;
+    cpuinfo >> freq;
+    return freq / 1000.0f; //convertir kHz en MHz
 }
 
-unsigned long MemoryMonitor::getUsedMemory() {
-    return static_cast<unsigned long>(RAM.usage * 1024); //en KB
-}
-
-double MemoryMonitor::getMemoryUsagePercentage() {
-    if (RAM.totalMemInMb == 0) return 0.0;
-    return (RAM.usage / RAM.totalMemInMb) * 100.0;
-}
-
-unsigned long MemoryMonitor::getTotalSwap() {
-    return RAM.SwapMemInMb * 1024; //en KB
-}
-
-unsigned long MemoryMonitor::getFreeSwap() {
-    return static_cast<unsigned long>(RAM.freeSwp * 1024); //en KB
-}
-
-unsigned long MemoryMonitor::getUsedSwap() {
-    return static_cast<unsigned long>(RAM.usageSwp * 1024); //en KB
-}
-
-double MemoryMonitor::getSwapUsagePercentage() {
-    if (RAM.SwapMemInMb == 0) return 0.0;
-    return (RAM.usageSwp / RAM.SwapMemInMb) * 100.0;
+string CpuMonitor::getCpuInfo() const {
+    //creer une chaine avec les infos cpu
+    ostringstream oss; //construire et formater une chaine de caracteres en sortie (flux de sortie vers une chaine de caracteres)
+    oss << "CPU Use: " << CPU.usageCPU << "% | Frequency: " << getCpuFreq() << " MHz";
+    return oss.str(); //convertit le contenu du flux oss en string et le retourne (fct str() de ostringstream)
 }
