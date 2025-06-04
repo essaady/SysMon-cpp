cf85f4214bc3d93aaecd90e014940f99bfaabd7a
majda rhoumriss <majda.rhoumriss.64@edu.uiz.ac.ma>
2025-06-04 12:48:40 +0200

    'edeting'

--- src/CpuMonitor.cpp
+++ src/CpuMonitor.cpp
@@ -1,5 +1,8 @@
+#ifndef CPUMONITOR_H
+#define CPUMONITOR_H
+#include "CpuMonitor.h"
 #include <iostream>
-#include <vector>
+
 
 class CpuMonitor {
 private:
@@ -11,39 +14,51 @@ private:
     } cpu;
 
 public:
-    CpuMonitor() {
-        cpu.nbrCPU = 4;  // Simulation avec 4 cœurs
-        cpu.usagePerCPU = new float[cpu.nbrCPU]{20.5f, 35.2f, 50.1f, 10.0f};
-        cpu.frequency = 2.4f;     
-        cpu.frequencyMax = 3.5f;   
-    }
+    CpuMonitor();
+    ~CpuMonitor();
 
-    ~CpuMonitor() {
-        delete[] cpu.usagePerCPU;
-    }
-
-    bool update() {
-        // Simuler une mise à jour des données CPU
-        for (int i = 0; i < cpu.nbrCPU; ++i) {
-            cpu.usagePerCPU[i] = 10.0f + i * 5.0f;  // Valeurs fictives
-        }
-        cpu.frequency = 2.5f;  // Simuler une fréquence actuelle
-        return true;
-    }
-
-    float getCpuUsage() {
-        float total = 0.0f;
-        for (int i = 0; i < cpu.nbrCPU; ++i) {
-            total += cpu.usagePerCPU[i];
-        }
-        return total / cpu.nbrCPU;  /
-    }
-
-    float getCpuFreq() {
-        return cpu.frequency;
-    }
-
-    float getCpuInfo() {
-        return cpu.frequencyMax;
-    }
+    bool update();               
+    float getCpuUsage();        
+    float getCpuFreq();        
+    float getCpuInfo();         
 };
+
+#endif
+
+
+
+CpuMonitor::CpuMonitor() {
+    cpu.nbrCPU = 4;  
+    cpu.usagePerCPU = new float[cpu.nbrCPU]{20.5f, 35.2f, 50.1f, 10.0f};
+    cpu.frequency = 2.4f;    
+    cpu.frequencyMax = 3.5f;  
+}
+
+CpuMonitor::~CpuMonitor() {
+    delete[] cpu.usagePerCPU;
+}
+
+bool CpuMonitor::update() {
+
+    for (int i = 0; i < cpu.nbrCPU; ++i) {
+        cpu.usagePerCPU[i] = 10.0f + i * 5.0f;
+    }
+    cpu.frequency = 2.5f; 
+    return true;
+}
+
+float CpuMonitor::getCpuUsage() {
+    float total = 0.0f;
+    for (int i = 0; i < cpu.nbrCPU; ++i) {
+        total += cpu.usagePerCPU[i];
+    }
+    return total / cpu.nbrCPU;
+}
+
+float CpuMonitor::getCpuFreq() {
+    return cpu.frequency;
+}
+
+float CpuMonitor::getCpuInfo() {
+    return cpu.frequencyMax;
+}
