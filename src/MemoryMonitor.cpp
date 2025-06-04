b1ffdb938707e8b439e317019a8e4abbb60b865e
mohamed yacine elfadili <elfadiliyacine@gmail.com>
2025-06-04 20:24:25 +0000

    update the /proc reading function in the MemoryMonitor.cpp , making in more simple

--- src/MemoryMonitor.cpp
+++ src/MemoryMonitor.cpp
@@ -16,67 +16,55 @@ MemoryMonitor::MemoryMonitor() {
 
 bool MemoryMonitor::update() {
     std::ifstream meminfo("/proc/meminfo");
-    if (!meminfo.is_open()) {
-        std::cerr << "Failed to open /proc/meminfo" << std::endl;
-        return false;
-    }
-
     std::string line;
-    unsigned long long memTotal = 0, memFree = 0, buffers = 0, cached = 0;
+
+    unsigned long long memTotal = 0, memFree = 0, memAvailable = 0;
     unsigned long long swapTotal = 0, swapFree = 0;
 
     while (std::getline(meminfo, line)) {
-        std::istringstream ss(line);
-        std::string key;
-        unsigned long long value;
-        std::string unit;
-
-        ss >> key >> value >> unit;
-
-        if (key == "MemTotal:") memTotal = value;
-        else if (key == "MemFree:") memFree = value;
-        else if (key == "Buffers:") buffers = value;
-        else if (key == "Cached:") cached = value;
-        else if (key == "SwapTotal:") swapTotal = value;
-        else if (key == "SwapFree:") swapFree = value;
+        if (line.compare(0, 9, "MemTotal:") == 0)
+            sscanf(line.c_str(), "MemTotal: %llu", &memTotal);
+        else if (line.compare(0, 13, "MemAvailable:") == 0)
+            sscanf(line.c_str(), "MemAvailable: %llu", &memAvailable);
+        else if (line.compare(0, 10, "SwapTotal:") == 0)
+            sscanf(line.c_str(), "SwapTotal: %llu", &swapTotal);
+        else if (line.compare(0, 9, "SwapFree:") == 0)
+            sscanf(line.c_str(), "SwapFree: %llu", &swapFree);
     }
-    meminfo.close();
 
-    unsigned long long usedMem = memTotal - memFree - buffers - cached;
-    unsigned long long usedSwap = swapTotal - swapFree;
-
+    // from KB to MB
     RAM.totalMemInMb = memTotal / 1024;
-    RAM.freeMem = memFree / 1024;
+    RAM.freeMem = memAvailable / 1024;
     RAM.SwapMeminMb = swapTotal / 1024;
     RAM.freeSwp = swapFree / 1024;
-    RAM.usage = usedMem / 1024;
-    RAM.usageSwp = usedSwap / 1024;
 
-    return true;
+    RAM.usage = RAM.totalMemInMb - RAM.freeMem;
+    RAM.usageSwp = RAM.SwapMeminMb - RAM.freeSwp;
+    return true; 
 }
 
 unsigned long long MemoryMonitor::getTotalMemory() {
-    return (RAM.totalMemInMb * 1024 * 1024);
+    return (RAM.totalMemInMb);
 }
 
 unsigned long long MemoryMonitor::getFreeMemory() {
-    return (RAM.freeMem * 1024 * 1024);
+    return (RAM.freeMem);
 }
 
 unsigned long long MemoryMonitor::getUsedMemory() {
-    return (RAM.usage * 1024 * 1024);
+    return (RAM.usage);
 }
 
 unsigned long long MemoryMonitor::getTotalSwap() {
-    return (RAM.SwapMeminMb * 1024 * 1024);
+    return (RAM.SwapMeminMb);
 }
 
 unsigned long long MemoryMonitor::getFreeSwap() {
-    return (RAM.freeSwp * 1024 * 1024);
+    return (RAM.freeSwp);
 }
 
 unsigned long long MemoryMonitor::getUsedSwap() {
-    return (RAM.usageSwp * 1024 * 1024);
+    return (RAM.usageSwp);
 }
 
 double MemoryMonitor::getMemoryUsagePercentage() {
