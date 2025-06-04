072a04f55d63711ac03dd778ec868352656059bb
Gdaa Jaber <jabergdaa18@gmail.com>
2025-06-04 14:57:27 +0100

    Update SysMon class

--- include/SysMon.h
+++ include/SysMon.h
@@ -1,18 +1,32 @@
 #ifndef SYSMON_H
 #define SYSMON_H
 
+#include <string>
 #include "CpuMonitor.h"
 #include "MemoryMonitor.h"
 #include "ProcessMonitor.h"
 
-extern int updateInterval;
-extern std::string exportFormat;
-extern bool help;
+class SysMon {
+private:
+    int updateInterval;
+    bool fullLog;
+    CpuMonitor cpuMonitor;
+    MemoryMonitor memoryMonitor;
+    ProcessMonitor processMonitor;
 
-std::string exportAsText();
-std::string exportAsCSV();
-bool update(CpuMonitor&, MemoryMonitor&, ProcessMonitor&);
-std::string getTime();
-std::string getInfo();
+public:
+    SysMon(int interval = 1000, bool fullLog = false);
+    
+    int run();
+    
+    std::string exportAsText();
+    std::string exportAsCSV();
+    
+    bool update();
+    
+    std::string getTime();
+    std::string getInfo(std::string infoType);
+    void log();
+};
 
-#endif
+#endif // SYSMON_H
