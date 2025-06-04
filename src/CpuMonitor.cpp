2878f43f943948aac56e2dc45be304e410d16319
Meryem-112 <meryemsofyan61@gmail.com>
2025-06-04 14:47:14 +0100

    CpuMonitor.h

--- src/CpuMonitor.cpp
+++ src/CpuMonitor.cpp
@@ -1,39 +1,27 @@
-#include "../include/CpuMonitor.h"
-#include <fstream>
-#include <sstream>
-#include <thread>
-#include <chrono>
+#ifndef CPUMONITOR_H
+#define CPUMONITOR_H
 
-CpuMonitor::CpuMonitor() {}
+#include <string>
 
-CpuMonitor::CpuTimes CpuMonitor::readCpuTimes() {
-    std::ifstream file("/proc/stat");
-    std::string line;
-    CpuTimes times{};
+class CpuMonitor {
+public:
+    struct CPU {
+        float usagePerCPU;
+        float frequency;
+        float frequencyMax;
+    };
 
-    if (std::getline(file, line)) {
-        std::istringstream iss(line);
-        std::string cpu;
-        iss >> cpu >> times.user >> times.nice >> times.system >> times.idle
-            >> times.iowait >> times.irq >> times.softirq >> times.steal;
-    }
+private:
+    short nbrCPU;
+    CPU cpu;
 
-    return times;
-}
+public:
+    CpuMonitor();
+    bool update();
+    float getCpuUsage();
+    float getCpuFreq();
+    std::string getCpuInfo();
+};
 
-double CpuMonitor::getCpuUsage() {
-    CpuTimes t1 = readCpuTimes();
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    CpuTimes t2 = readCpuTimes();
+#endif // CPUMONITOR_H
 
-    long long total1 = t1.total();
-    long long total2 = t2.total();
-    long long idle1 = t1.idleTime();
-    long long idle2 = t2.idleTime();
-
-    long long deltaTotal = total2 - total1;
-    long long deltaIdle = idle2 - idle1;
-
-    if (deltaTotal == 0) return 0.0;
-    return 100.0 * (deltaTotal - deltaIdle) / deltaTotal;
-}
